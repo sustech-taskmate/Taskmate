19d69cb30019ce8eecc0304989e912815d8f8ba5
Tavish <12011618@mail.sustech.edu.cn>
2023-03-18 12:24:51 +0800

    add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import {resolve} from 'path';
 
 const mobile =
   process.env.TAURI_PLATFORM === "android" ||
@@ -27,5 +28,8 @@ export default defineConfig(async () => ({
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
   },
 }));
