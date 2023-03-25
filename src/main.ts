98af60b00ffee34667f35c8afa992e5871f9bf67
wind <12012211@mail.sustech.edu.cn>
2023-03-25 23:01:01 +0800

    merged

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,18 @@ import Ele from 'element-plus'
 import 'element-plus/dist/index.css'
 import VueVideoPlayer from 'vue-video-player'
 import 'video.js/dist/video-js.css'
+// @ts-ignore
 import VMdPreview from '@kangc/v-md-editor/lib/preview';
 import '@kangc/v-md-editor/lib/style/preview.css';
+// @ts-ignore
 import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
 import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
+// @ts-ignore
 import createLineNumbertPlugin from '@kangc/v-md-editor/lib/plugins/line-number/index';
+// @ts-ignore
 import createCopyCodePlugin from '@kangc/v-md-editor/lib/plugins/copy-code/index';
 import '@kangc/v-md-editor/lib/plugins/copy-code/copy-code.css';
+// @ts-ignore
 import createEmojiPlugin from '@kangc/v-md-editor/lib/plugins/emoji/index';
 import '@kangc/v-md-editor/lib/plugins/emoji/emoji.css';
 import Prism from 'prismjs';
