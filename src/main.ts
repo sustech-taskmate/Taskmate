34f886be746938497e15f0b6ac408a94d9cb5561
wind <12012211@mail.sustech.edu.cn>
2023-03-25 22:51:10 +0800

    add mp4 format, temporarily fix img

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,7 @@ import router from './router'
 import store from './store'
 import Ele from 'element-plus'
 import 'element-plus/dist/index.css'
+import VueVideoPlayer from 'vue-video-player'
+import 'video.js/dist/video-js.css'
 
-createApp(App).use(store).use(Ele).use(router).mount('#app')
+createApp(App).use(store).use(Ele).use(router).use(VueVideoPlayer).mount('#app')
--- src/view/Assign.vue
+++ src/view/Assign.vue
@@ -13,8 +13,18 @@
           class="image"
           :src="'src/assets/test.png'"
           :preview-src-list="['src/assets/test.png']"
+          style="position: absolute; left: 0; top: 25%"
       >
       </el-image>
+
+      <video-player v-if="format==='mp4'" class="video-player vjs-custom-skin"
+                    ref="videoPlayer"
+                    :playsinline="true"
+                    :options="playerOptions"
+                    style="position: absolute; left: 0; top: 25%"
+      >
+      </video-player>
+
     </div>
     <div class="right">
       right
@@ -23,6 +33,7 @@
 </template>
 
 <script>
+
 export default {
   name: 'Assign',
   created() {
@@ -31,16 +42,40 @@ export default {
   },
   data() {
     return {
-      format: 'office',
+      format: 'mp4',
       pdf_url: 'src/assets/test.pdf',
       img_url: 'src/assets/test.png',
       office: '',
       office_url: 'http://view.officeapps.live.com/op/view.aspx?src=' + this.office,
+      playerOptions: {
+        playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
+        autoplay: true, //如果true,浏览器准备好时开始回放。
+        muted: false, // 默认情况下将会消除任何音频。
+        loop: true, // 导致视频一结束就重新开始。
+        preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+        language: 'zh-CN',
+        aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+        sources: [{
+          src: 'src/assets/test.mp4',  // 路径
+          type: 'video/mp4'  // 类型
+        }, {
+          src: 'src/assets/test.mp4',
+          type: 'video/webm'
+        }],
+        notSupportedMessage: '此视频暂无法播放，请稍后再试', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+        controlBar: {
+          timeDivider: true,
+          durationDisplay: true,
+          remainingTimeDisplay: false,
+          fullscreenToggle: true  //全屏按钮
+        },
+        controls: true
+      }
     }
   },
-  methods: {
-
-  }
+  methods: {},
+  computed: {}
 }
 </script>
 
@@ -87,6 +122,6 @@ body {
   left: 70%;
 }
 
-.image{
+.image {
 }
 </style>
