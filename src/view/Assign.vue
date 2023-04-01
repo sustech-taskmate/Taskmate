97868f4a4112ff2658c8a17759fb70d8855ad19a
SnowCharm <619022098@qq.com>
2023-04-01 21:24:35 +0800

    :bug: fix(Assign.vue): fix video playing
    playing video when choosing video item

--- src/view/Assign.vue
+++ src/view/Assign.vue
@@ -5,12 +5,8 @@
           class="el-menu-vertical-demo"
           @select="handleSelect"
           v-for="item in fileMap.values()"
+          :collapse="true"
       >
-        <tree-menu v-if="item.format===AssignFileType.zip" :nodes="this.zipNode">
-          <el-icon>
-            <Document/>
-          </el-icon>
-        </tree-menu>
         <el-menu-item v-if="item.format!==AssignFileType.zip" :index="item.filename">
           <el-icon>
             <Document/>
@@ -53,12 +49,11 @@
 
 <script setup lang="ts">
 import {AssignFile, AssignFileType} from '@/store/assign';
-import TreeMenu from "@/components/TreeMenu.vue";
 import {ref} from "vue";
 
 let fileMap = new Map<string, AssignFile>([
-  ['test.zip', new AssignFile('test.zip', AssignFileType.zip,
-      'src/assets/test.zip', undefined)],
+  // ['test.zip', new AssignFile('test.zip', AssignFileType.zip,
+  //     'src/assets/test.zip', undefined)],
   ['test.mp4', new AssignFile('test.mp4', AssignFileType.video,
       'src/assets/test.mp4', undefined)],
   ['test.pdf', new AssignFile('test.pdf', AssignFileType.pdf,
@@ -68,6 +63,7 @@ let fileMap = new Map<string, AssignFile>([
 ])
 let format = ref(AssignFileType.placeholder);
 let url = ref("");
+
 const handleSelect = (key: string, keyPath: string[]) => {
   let filename = key.split("\n")[0];
   let file = fileMap.get(filename);
@@ -86,6 +82,32 @@ const handleSelect = (key: string, keyPath: string[]) => {
     url.value = file.url;
   }
 }
+
+let playerOptions = ref({
+  playbackRates: [0.7, 1.0, 1.25, 1.5, 2.0], //播放速度
+  autoplay: true, //如果true,浏览器准备好时开始回放。
+  muted: false, // 默认情况下将会消除任何音频。
+  loop: true, // 导致视频一结束就重新开始。
+  preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+  language: 'en',
+  aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+  fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+  sources: [{
+    src: url,  // 路径
+    type: 'video/mp4'  // 类型
+  }, {
+    src: url,
+    type: 'video/webm'
+  }],
+  notSupportedMessage: 'The video can not be played, please try again later', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+  controlBar: {
+    timeDivider: true,
+    durationDisplay: true,
+    remainingTimeDisplay: false,
+    fullscreenToggle: true  //全屏按钮
+  },
+  controls: true
+});
 
 </script>
 
@@ -112,32 +134,6 @@ export default defineComponent({
   },
   data() {
     return {
-      video: '',
-      playerOptions: {
-        playbackRates: [0.7, 1.0, 1.25, 1.5, 2.0], //播放速度
-        autoplay: true, //如果true,浏览器准备好时开始回放。
-        muted: false, // 默认情况下将会消除任何音频。
-        loop: true, // 导致视频一结束就重新开始。
-        preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-        language: 'en',
-        aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
-        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-        sources: [{
-          src: this.video,  // 路径
-          type: 'video/mp4'  // 类型
-        }, {
-          src: this.video,
-          type: 'video/webm'
-        }],
-        notSupportedMessage: 'The video can not be played, please try again later', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
-        controlBar: {
-          timeDivider: true,
-          durationDisplay: true,
-          remainingTimeDisplay: false,
-          fullscreenToggle: true  //全屏按钮
-        },
-        controls: true
-      },
       zipNode: [] as FileTreeNode[],
     }
   },
