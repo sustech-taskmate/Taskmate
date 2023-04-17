f7fd52ab52d13f9f0a731f01dc16a06fa9681358
SnowCharm <619022098@qq.com>
2023-04-17 12:39:13 +0800

    :sparkles: feat(AssignRightBar.vue): add submit and next buttons

--- src/components/AssignRightBar.vue
+++ src/components/AssignRightBar.vue
@@ -135,6 +135,12 @@
       </el-row>
     </el-row>
   </div>
+  <div class="btn-box">
+    <el-row>
+      <button type="button" class="btn submit">Submit</button>
+      <button type="button" class="btn next">Next</button>
+    </el-row>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -249,5 +255,43 @@ export default {
   overflow-y: auto;
 }
 
+.btn-box {
+  height: 12%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
+.btn {
+  border: 1px solid;
+  background-color: transparent;
+  text-transform: uppercase;
+  font-size: calc(100vw * 24 / 1500);
+  font-weight: 300;
+}
+
+.btn:hover {
+  color: white;
+  border: 0;
+  cursor: pointer;
+}
+
+.submit {
+  color: #f038ff;
+}
+
+.submit:hover {
+  background-color: #f038ff;
+  -moz-box-shadow: 10px 10px 99px 6px rgba(240, 56, 255, 1);
+}
+
+.next {
+  color: teal;
+  margin-left: calc(100vw * 10 / 1500);
+}
+
+.next:hover {
+  background-color: teal;
+  -moz-box-shadow: 10px 10px 99px 6px rgba(0, 128, 128, 1);
+}
 </style>
