3bc5788f59536ceadfd8c8648e920ff28c322583
SnowCharm <619022098@qq.com>
2023-04-22 13:26:59 +0800

    :lipstick: feat(global style): add some global styles

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,6 @@
 import './style/global/scrollbar.css';
 import './style/global/body.css'
+import './style/global/animation.scss'
+import './style/global/helper.scss'
 
 export default function setupStyle() {}
