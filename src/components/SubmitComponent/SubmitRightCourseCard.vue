46305b8fd1fd29f2dd34b0915ad91bb5ed02db94
wind <12012211@mail.sustech.edu.cn>
2023-05-09 12:10:29 +0800

    ✨ feat(student view): add student interface

--- src/components/SubmitComponent/SubmitRightCourseCard.vue
+++ src/components/SubmitComponent/SubmitRightCourseCard.vue
@@ -1,13 +1,71 @@
 <template>
-$END$
+    <el-col :span="24" style="height: 100%; width: 98%">
+        <el-row style="height: 10vh" @click="rotate(CourseData)">
+            <el-col :span="21" style="height: 100%; display: flex; align-items: center; justify-content: center; border-radius: 0 0 0 0;
+                                  border-bottom: 2px solid black; border-top: 2px solid black;">
+                {{ CourseData.name }}
+            </el-col>
+            <el-col :span="3" style="height: 100%; display: flex; align-items: center; justify-content: center; border-radius: 0 0 0 0;
+                                  border-bottom: 2px solid black; border-top: 2px solid black;">
+                <svg x="1682076109200" class="icon rotate" :class="{active: CourseData.down}" viewBox="0 0 1024 1024"
+                     version="1.1"
+                     xmlns="http://www.w3.org/2000/svg" p-id="2609" width="20" height="20"
+                     style="transform-box:fill-box; transform-origin:center; rotate: 180deg"
+                >
+                    <path d="M93.37191 57.154573 930.62809 540.635608 93.37191 1024Z" fill="#272636" p-id="2610">
+                    </path>
+                </svg>
+            </el-col>
+        </el-row>
+        <el-row class="downList" v-if="CourseData.down">
+            <div style="display: flex; flex-wrap: wrap; width: 100%; align-items: center;" v-for="context in CourseData.assignmentList">
+                <el-row style="height: 7vh; width: 100%; flex: 1 0 30%; border-bottom:  2px solid black;
+                               display: flex; justify-content: center; align-items: center;" @click="changeContext(context, CourseData.name)"
+                >
+                        {{context.name}}
+                </el-row>
+            </div>
+        </el-row>
+    </el-col>
 </template>
 
-<script>
-export default {
-name: "SubmitRightCourseCard"
+
+<script lang="ts" setup>
+
+import {Assignment, Course} from "@/store/submit";
+
+const rotate = (item: Course) => {
+    item.down = !item.down
 }
 </script>
 
-<style scoped>
+<script lang="ts">
+import {Assignment, Course} from "@/store/submit";
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: "SubmitRightCourseCard",
+    props: {
+        CourseData: {
+            type: Course,
+            required: true
+        },
+    },
+    methods: {
+        changeContext (context: Assignment, courseName: string){
+            console.log("aaa")
+            this.$emit('child-event', context);
+            this.$emit('order', courseName, context.name)
+        }
+    }
+})
+</script>
 
+<style scoped>
+.rotate {
+    transition: transform 0.5s;
+}
+
+.rotate.active {
+    transform: rotate(-90deg);
+}
 </style>
---
+++ src/store/submit.ts
@@ -0,0 +1,56 @@
+class AttachFile{
+    name: string;
+    size: number;
+    time: string;
+    constructor(name:string, size: number, time: string) {
+        this.name = name
+        this.size = size
+        this.time = time
+    }
+}
+
+
+/**
+ * 每一项作业
+ * name: 作业名称
+ * ddl: 截止时间
+ * lateTime: 迟交时间
+ * submitTime: 最近提交时间
+ * attachment: 作业附件
+ * */
+class Assignment{
+    name: string;
+    ddl: Date;
+    lateTime: Date;
+    submitTime: Date | null;
+    attachment: AttachFile[] | null;
+    constructor(name: string, ddl: Date, submitTime: Date | null, lateTime: Date, attachment: AttachFile[] | null) {
+        this.name = name;
+        this.ddl = ddl;
+        this.submitTime = submitTime;
+        this.lateTime = lateTime;
+        this.attachment = attachment;
+    }
+}
+
+interface Assignment {
+    prototype: any;
+}
+
+/**
+ * name: 课程名称
+ * assignmentList: 作业列表
+ * down: 这个列表是否拉开
+ * */
+class Course{
+    name: string;
+    assignmentList: Assignment[];
+    down: boolean;
+    constructor(name: string, assignmentList: Assignment[], down: boolean) {
+        this.name = name;
+        this.assignmentList = assignmentList
+        this.down = down;
+    }
+}
+
+export {Assignment, Course, AttachFile}
