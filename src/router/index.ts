88ef70b302dc14d826cf3b198cc29405a5db7691
Tavish <12011618@mail.sustech.edu.cn>
2023-05-16 09:05:58 +0800

    fix(auth): add unauthenticated pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,56 +1,69 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {createRouter, createWebHashHistory, NavigationGuardNext, RouteLocationNormalized} from 'vue-router'
 import {App} from "vue";
 
 const routes = [
-  {
-    path: '/',
-    name: 'index',
-    component: () => import('../view/Index.vue')
-  },
-  {
-    path: '/loading',
-    name: 'loading',
-    component: () => import('../view/Loading.vue')
-  },
-  {
-    path: '/grade/t/:cid',
-    name: 'teacherCourse',
-    component: () => import('../view/CourseView.vue')
-  },
-  {
-    path: '/grade/t/:cid/:aid',
-    name: 'teacherAssign',
-    component: () => import('../view/AssignView.vue')
-  },
-  {
-    path: '/grade/t/:cid/:aid/statistics',
-    name: 'statistics',
-    component: () => import('../view/AssignStatistics.vue')
-  },
-  // {
-  //   path: '/Main/Set',
-  //   component: () => import('../view/AssignSetting.vue')
-  // },
-  {
-    path: '/grade/t/:cid/:aid/:gid',
-    name: 'grade',
-    component: () => import('../view/Grading.vue'),
-    children: [{
-      path: 'file/',
-      name: 'render',
-      component: () => import('../components/AssignContent/FileContent.vue')
-    }]
-  },
+    {
+        path: '/',
+        name: 'index',
+        component: () => import('../view/Index.vue'),
+        beforeEnter: ((to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
+            if (localStorage.getItem('token')) {
+                next()
+            } else {
+              next({
+                name: 'empty',
+              })
+            }
+        })
+    },
+    {
+        path: '/loading',
+        name: 'loading',
+        component: () => import('../view/Loading.vue')
+    },
+    {
+        path: '/empty',
+        name: 'empty',
+        component: () => import('../view/Empty.vue')
+    },
+    {
+        path: '/grade/t/:cid',
+        name: 'teacherCourse',
+        component: () => import('../view/CourseView.vue')
+    },
+    {
+        path: '/grade/t/:cid/:aid',
+        name: 'teacherAssign',
+        component: () => import('../view/AssignView.vue')
+    },
+    {
+        path: '/grade/t/:cid/:aid/statistics',
+        name: 'statistics',
+        component: () => import('../view/AssignStatistics.vue')
+    },
+    // {
+    //   path: '/Main/Set',
+    //   component: () => import('../view/AssignSetting.vue')
+    // },
+    {
+        path: '/grade/t/:cid/:aid/:gid',
+        name: 'grade',
+        component: () => import('../view/Grading.vue'),
+        children: [{
+            path: 'file/',
+            name: 'render',
+            component: () => import('../components/AssignContent/FileContent.vue')
+        }]
+    },
 
 ]
-
 export const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-  scrollBehavior: () => ({left:0, top:0})
+    history: createWebHashHistory(),
+    routes,
+    scrollBehavior: () => ({left: 0, top: 0})
 })
 
 export async function setupRouter(app: App) {
-  app.use(router);
-  await router.isReady();
+    app.use(router);
+    await router.isReady();
 }
---
+++ src/view/Empty.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Unauthenticated!</h1>
+</template>
+
+<script lang="ts" setup>
+import {useRouterPush} from "@/composable";
+import {request} from "@/service/request";
+import type {Service} from "@/typing/system";
+import {Body} from "@tauri-apps/api/http";
+import {loginAuth} from '@/util'
+
+await loginAuth()
+
+let listener = async function (e: any) {
+  if (e.detail && e.detail.key == 'token' && e.detail.value) {
+    let token = e.detail.value
+    const {routerPush} = useRouterPush();
+    await request.post("https://spaces.sustech.cloud/api/auth/login",
+        {
+          body: Body.json({
+            token: token
+          })
+        } as Service.RequestOption);
+    await routerPush({name: 'index'})
+  }
+  window.removeEventListener("setToken", listener)
+}
+
+window.addEventListener('setToken', listener)
+</script>
+
+<style scoped>
+
+</style>
