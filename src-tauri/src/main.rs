f120ebc0c9b134c71837ed84a226df79fbb60646
Tavish <12011618@mail.sustech.edu.cn>
2023-05-16 09:00:52 +0800

    feat(communication): add global event for the frontend and the backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,44 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
+use std::env;
+use std::process;
+use std::process::Command;
+
+use tauri::{*};
+use url::Url;
+
 mod file_handler;
 mod protocol;
 mod tray;
+mod browser;
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+    token: String,
+}
 
 fn main() {
+    // protocol::reg_url_protocol();
+    tauri_plugin_deep_link::prepare("taskmate");
     tauri::Builder::default()
+        .setup(|app| {
+            let handle = app.handle();
+            tauri_plugin_deep_link::register(
+                "taskmate",
+                move |request| {
+                    let query = Url::parse(request.as_str()).unwrap();
+                    let query_map = query.query_pairs().into_owned().collect::<std::collections::HashMap<_, _>>();
+                    let token = query_map.get("token").unwrap().to_string();
+                    handle.emit_all("authenticate", Payload { token }).unwrap();
+                },
+            );
+            app.once_global("authenticate", |event| { browser::open_browser("https://spaces.sustech.cloud/"); });
+            Ok(())
+        })
         .system_tray(tray::menu())
         .on_system_tray_event(tray::handler)
-        .invoke_handler(tauri::generate_handler![protocol::reg_url_protocol,file_handler::download_file,file_handler::analyze_dir])
+        .invoke_handler(tauri::generate_handler![file_handler::download_file,file_handler::analyze_dir])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
