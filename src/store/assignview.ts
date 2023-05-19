b345ea3f739c6c0add53f610d5dac5b768d7a5f9
SnowCharm <619022098@qq.com>
2023-05-19 15:43:42 +0800

    :lipstick: style(AssignView): improve AssignView UI design

---
+++ src/store/assignview.ts
@@ -0,0 +1,18 @@
+export enum AssignmentState {
+    notModified,
+    modifying,
+    finished
+}
+
+export interface StudentContent {
+    gid: string,
+    sid: string,
+    submitTime: string | null,
+    modifyState: AssignmentState,
+    lastModifiedBy: string,
+    lastTime: string | null,
+    score: number,
+
+    //数据可视化
+    //名字，release，ddl，延期时间，提交比，评分比，是否return，setting 三个时间支持排序，return排序，
+}
--- src/view/AssignView.vue
+++ src/view/AssignView.vue
@@ -32,19 +32,21 @@
       </div>
       <div style="height: 100vh; position: relative">
         <div style="background-color: steelblue; color: white; height: 10vh; align-items: center">
-          <span class="word" style="line-height: 3vh; position: absolute; left: 3vw; top: 3vh;">作业信息</span>
+          <span class="word" style="line-height: 3vh; position: absolute; left: 3vw; top: 3vh;">Assignments Information</span>
         </div>
-        <div style="left: 0; top: 2vh; position: relative">
+        <div style="left: 0; top: 1vh; position: relative">
           <span class="word"
-                style="line-height: 3vh; position: absolute; left: 2.5vw; top: 2vh;">{{ assignments[Number(aid)].name }}</span>
-          <div style="width: 27vw; height: 7vh; position: absolute; right: 0; top: 10vh;">
+                style="line-height: 3vh; position: absolute; left: 2.5vw; top: 2vh;font-size: calc(100vw * 48 / 1500)">
+              {{ assignments[Number(aid)].name }}
+          </span>
+          <div style="width: 20vw; height: 7vh; position: absolute; right: 0; top: 8vh;">
             <svg-icon name="download" style="width: 6vw; height: 6vh; cursor: pointer"></svg-icon>
             <svg-icon name="transmit" style="width: 6vw; height: 6vh; cursor: pointer"></svg-icon>
             <svg-icon name="signal" style="width: 6vw; height: 6vh; cursor: pointer" @click="toStatistics"></svg-icon>
 <!--            <svg-icon name="set" style="width: 7vw; height: 7vh" @click="router.push('/Main/Set')"></svg-icon>-->
           </div>
         </div>
-        <div style="position: relative; height: 68vh; top: 22vh" v-bind:style="{ width: rightWidth }">
+        <div style="position: relative; height: 68vh; top: 18vh" v-bind:style="{ width: rightWidth }">
           <div style="background-color: steelblue; height: 10vh">
             <span class="word" style="line-height: 3vh; color: white; position: absolute; left: 2.5vw; top: 4vh;">Student List</span>
             <svg-icon name="play2" color="white"
@@ -53,17 +55,18 @@
           <el-table
               :data="tableData"
               :row-style="getRowStyle"
-              :header-row-style="{height: '10vh', background: 'pink'}"
+              :header-row-style="{height: '8vh', fontSize: '14px'}"
+              height="100%"
           >
-            <el-table-column type="selection" v-if="true"></el-table-column>
-            <el-table-column prop="sid" sortable label="学号" width="auto" align="center"/>
-            <el-table-column prop="score" sortable label="分数" width="auto" align="center"/>
-            <el-table-column prop="submitTime" sortable label="提交时间" width="auto" align="center"/>
-            <el-table-column prop="lastModifiedBy" label="上次修改者" width="auto" align="center"/>
-            <el-table-column prop="lastTime" label="上次修改时间" width="auto" align="center"/>
-            <el-table-column label="查看附件" width="auto" align="center">
+            <el-table-column type="selection"></el-table-column>
+            <el-table-column prop="sid" sortable label="Student ID" width="auto" align="center"/>
+            <el-table-column prop="score" sortable label="Score" width="auto" align="center"/>
+            <el-table-column prop="submitTime" sortable label="Submission Time" width="auto" align="center"/>
+            <el-table-column prop="lastModifiedBy" label="Last Modifier" width="auto" align="center"/>
+            <el-table-column prop="lastTime" label="Last Modification Time" width="auto" align="center"/>
+            <el-table-column width="auto" align="center">
               <template v-slot="scope">
-                <el-button type="primary" @click="toGrade(scope)">批改</el-button>
+                <el-button type="primary" @click="toGrade(scope)">GRADE</el-button>
               </template>
             </el-table-column>
           </el-table>
@@ -74,46 +77,6 @@
 
 </template>
 
-<style scoped>
-a {
-  color: black;
-}
-
-@media screen and (min-width: 1001px) {
-  .word {
-    font-size: 2.5vw;
-  }
-
-  .table-word {
-    font-size: 2vw;
-    margin-right: 2vw;
-  }
-}
-
-@media screen and (min-width: 601px) and (max-width: 1000px) {
-  .word {
-    font-size: 3vw;
-  }
-
-  .table-word {
-    font-size: 2.5vw;
-    margin-right: 0.5vw;
-  }
-}
-
-@media screen and (max-width: 600px) {
-  .word {
-    font-size: 3.5vw;
-  }
-}
-
-#container {
-  display: flex;
-  border: 2px solid black;
-  border-radius: 5px;
-}
-</style>
-
 <script setup lang="ts">
 import {reactive, ref, watch} from "vue";
 import SvgIcon from "@/components/util/SvgIcon.vue";
@@ -121,6 +84,8 @@ import {useRoute} from "vue-router";
 import {useRouterPush} from "@/composable";
 import {Assignment, getProblems, getSubmissions} from "@/composable/serverRequest";
 import moment from "moment";
+import {StudentContent, AssignmentState} from "@/store/assignview";
+import '@/assets/style/local/assignview.css'
 
 const route = useRoute();
 const {routerPush} = useRouterPush();
@@ -159,25 +124,6 @@ const toGrade = (scope: any) => {
   let gid = scope.row.gid;
   routerPush({name: 'grade', params: {cid: cid, aid: aid, gid: gid},
       query: {assignments: route.query.assignments, courses: route.query.courses}});
-}
-
-enum AssignmentState {
-  notModified,
-  modifying,
-  finished
-}
-
-interface StudentContent {
-  gid: string,
-  sid: string,
-  submitTime: string | null,
-  modifyState: AssignmentState,
-  lastModifiedBy: string,
-  lastTime: string | null,
-  score: number,
-
-  //数据可视化
-  //名字，release，ddl，延期时间，提交比，评分比，是否return，setting 三个时间支持排序，return排序，
 }
 
 let aid = ref(route.params.aid as string)
